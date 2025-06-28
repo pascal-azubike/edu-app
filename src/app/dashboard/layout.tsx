875a00d875f61776f2671acd3ad60d63c2264201
Pascal <pascalazubike004@gmail.com>
2025-06-28 14:55:08 -0700

    layout changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,6 +17,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
 }
 
 async function verifyUserOrRedirect() {
+	// commets
 	const cookieStore = await cookies();
 	console.log(cookieStore);
 	const authToken = cookieStore.get('access_token');
